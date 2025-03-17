a6e6721156d82106a7ec93de19ca37d4f0f58331
salih <salihedneer.mec@gmail.com>
2025-03-17 03:54:13 -0700

    build change

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the React build folder
-app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
 // Add this line after your middleware setup
@@ -105,12 +103,6 @@ app.post('/api/run', async (req, res) => {
   }
 });
 
-
-
-// For all other routes, serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 
 
